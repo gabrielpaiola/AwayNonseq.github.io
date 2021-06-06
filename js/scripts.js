f566762d8608476499164dcf7293444204718763
AwayNonseq <gcpaiola@hotmail.com>
2021-06-06 11:34:26 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,22 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+const handleClickOutside = (event) => {
+    let overlay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    if (!modal.contains(event.target)) {
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+        document.removeEventListener('click', handleClickOutside, false);
+    }
+}
+
+const modal = () => {
+    let overlay = document.getElementById("overlay");
+    let modal = document.getElementById("modal");
+    overlay.style.display = 'flex'
+    modal.style.display = 'flex'
+    setTimeout(() => { document.addEventListener('click', handleClickOutside, false) }, 200);
+}
