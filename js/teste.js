d6c491aa11e031ed6545dbbbcd6894438212e45f
AwayNonseq <gcpaiola@hotmail.com>
2021-06-23 15:47:59 -0300

    Update teste.js

--- js/teste.js
+++ js/teste.js
@@ -10,7 +10,7 @@
             cfg = $("#float_dc").data(),
             iframe = 'https://discordapp.com/widget?id=843599777101774848&theme=dark',
             params = [],
-            png = $("#float_dc").attr('src').replace(/\?.*/, '').replace(/[\w\.]+$/, "discord2.png")
+            png = $("discord2.png").attr('src').replace(/\?.*/, '').replace(/[\w\.]+$/, "aa")
         ;
         var val;
         for(var k in defaults)
